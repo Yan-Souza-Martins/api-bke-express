fd05b793cacdded9a01e1cd0cef5214882dee77f
Yan Souza <yan.s@aluno.ifsp.edu.br>
2024-08-06 17:46:08 -0300

    Criação das Rotas no server.js

--- src/server.js
+++ src/server.js
@@ -2,14 +2,35 @@ import express from "express"
 
 const app = express()
 
-app.get('/', (req, res) =>{
-    res.json(
-        {
-            massage: "Olá Mundo Express!",
-            success: "Sucesso ao executar API"
-        }
-    )
+app.post('/auth/signup', (req, res) =>{
+    res.json({massage: "Rota de POST Auth/Signup",}) //Autenticação cadastro
 })
+
+app.post('/auth/login', (req, res) =>{
+    res.json({massage: "Rota de POST Auth/Login",}) //Autenticação login
+})
+
+app.post('/auth/logout', (req, res) =>{
+    res.json({massage: "Rota de POST Auth/Logout",}) //Autenticação sair da conta
+})
+
+
+app.post('/account/', (req, res) => {
+    res.json({massege: "Rota de POST /Account/"}) //Entrar na conta
+})
+app.get('/account/list', (req, res) => {
+    res.json({massege: "Rota GET /Account/List"}) //Lista de contas
+})
+app.get('/account/:id', (req, res) => {
+    res.json({massege: "Rota GET /Account/:id"}) //Chamar conta pelo id
+})
+app.put('/account/:id', (req, res) => {
+    res.json({massege: "Rota PUT /Account/:id"}) //Atualizar conta
+})
+app.delete('/account/:id', (req, res) => {
+    res.json({massege: "Rota DELETE /Account/:id"}) //Deletar conta
+})
+
 
 app.listen(3000, () =>{
     console.log('Servidor Rodando em http://localhost:3000')
