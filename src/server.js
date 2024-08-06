8b95c5512944a69df185a15dc1cd32f11c7879f8
Yan Souza <yan.s@aluno.ifsp.edu.br>
2024-08-06 15:52:06 -0300

    add lib express

--- src/server.js
+++ src/server.js
@@ -1,1 +1,16 @@
-console.log('Hello World')
+import express from "express"
+
+const app = express()
+
+app.get('/', (req, res) =>{
+    res.json(
+        {
+            massage: "Olá Mundo Express!",
+            success: "Sucesso ao executar API"
+        }
+    )
+})
+
+app.listen(3000, () =>{
+    console.log('Servidor Rodando em http://localhost:3000')
+})
