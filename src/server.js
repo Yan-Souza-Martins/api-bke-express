d3675d9a8903f24a9ced5a13423e454f1b7812d5
Yan Souza <yan.s@aluno.ifsp.edu.br>
2024-08-20 17:18:55 -0300

    add HOST e PORT env HAHAHAHA É OS D VIDA

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 import express from "express"
 import authRouter from './routers/authRouter.js'
 import accountRouter from './routers/accountRouter.js'
-import { ENVIRONMENT } from "./config.js"
+import { ENVIRONMENT, PORT, HOST } from "./config.js"
 
 const app = express()
 
@@ -10,6 +10,6 @@ app.use('/auth', authRouter)
 app.use('/account', accountRouter)
 
 
-app.listen(process.env.PORT,  () =>{
-    console.log(`Servidor Rodando no Ambiente ${ENVIRONMENT}`)
+app.listen(PORT,  () =>{
+    console.log(`Servidor Rodando no Ambiente ${ENVIRONMENT} em ${HOST}:${PORT}`)
 })
